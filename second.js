b104e1e2a995818d0c4417f9332539f91a74a848
endokishan <endokishan@gmail.com>
2020-12-30 23:54:20 +0530

    added some code in endo branch

--- second.js
+++ second.js
@@ -7,3 +7,8 @@
 // lets merge this endo branch with main without fast forward method
 
 // This is endo branch and this is first code of endo branch after cloning from main branch
+// this example of rebase 
+// 1st work  = code in endo branch
+// commit it
+// switch to main branch and code their without pushing any thing
+// and commit in main branch
